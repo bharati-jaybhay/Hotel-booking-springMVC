e547fbfee7e16d9e455b072aed871b0c9008a0da
Bharati-jaybhay <155735053+bharati-jaybhay@users.noreply.github.com>
2025-01-23 10:12:50 +0530

    update sprk_hotels directory

--- sprk_hotels/src/main/java/com/sprk/sprk_hotels/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.sprk.sprk_hotels.service;
-
-import com.sprk.sprk_hotels.model.Listing;
-
-import java.util.List;
-
-public interface UserService {
-    List<Listing> getAllListingsForUSer();
-}
